1742a4293d0d5e304fe26397e42d6171bb89ba07
nesma-shafie <nesmashafie342@gmail.com>
2024-04-21 20:59:41 +0200

    req 8 Entities and components

--- source/common/ecs/entity.cpp
+++ source/common/ecs/entity.cpp
@@ -4,29 +4,42 @@
 
 #include <glm/gtx/euler_angles.hpp>
 
-namespace our {
+namespace our
+{
 
     // This function returns the transformation matrix from the entity's local space to the world space
     // Remember that you can get the transformation matrix from this entity to its parent from "localTransform"
     // To get the local to world matrix, you need to combine this entities matrix with its parent's matrix and
     // its parent's parent's matrix and so on till you reach the root.
-    glm::mat4 Entity::getLocalToWorldMatrix() const {
-        //TODO: (Req 8) Write this function
-        return glm::mat4(1.0f);
+    glm::mat4 Entity::getLocalToWorldMatrix() const
+    {
+        // TODO: (Req 8) Write this function
+        Entity *element = this->parent;
+        glm::mat4 transformation_matrix = this->localTransform.toMat4();
+        while (element)
+        {
+            transformation_matrix = element->localTransform.toMat4() * transformation_matrix;
+            element = element->parent;
+        }
+        return transformation_matrix;
     }
 
     // Deserializes the entity data and components from a json object
-    void Entity::deserialize(const nlohmann::json& data){
-        if(!data.is_object()) return;
+    void Entity::deserialize(const nlohmann::json &data)
+    {
+        if (!data.is_object())
+            return;
         name = data.value("name", name);
         localTransform.deserialize(data);
-        if(data.contains("components")){
-            if(const auto& components = data["components"]; components.is_array()){
-                for(auto& component: components){
+        if (data.contains("components"))
+        {
+            if (const auto &components = data["components"]; components.is_array())
+            {
+                for (auto &component : components)
+                {
                     deserializeComponent(component, this);
                 }
             }
         }
     }
-
 }
