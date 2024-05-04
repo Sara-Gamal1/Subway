9b43d560f665de830356304ba5017c998c7c1411
nesma-shafie <nesmashafie342@gmail.com>
2024-05-04 22:57:55 +0300

    ✨ 3 levels with different speeds

--- source/common/ecs/world.cpp
+++ source/common/ecs/world.cpp
@@ -1,4 +1,7 @@
 #include "world.hpp"
+#include "../common/components/movement.hpp"
+#include "../common/components/player.hpp"
+#include <iostream>
 
 namespace our
 {
@@ -16,6 +19,17 @@ namespace our
             Entity *element = this->add();
             element->parent = parent;
             element->deserialize(entityData);
+            if (element->getComponent<MovementComponent>() && element->getComponent<PlayerComponent>())
+            {
+                MovementComponent *move = element->getComponent<MovementComponent>();
+                move->linearVelocity = glm::vec3(0, 0, -5 *2* this->level);
+            }
+
+            if (element->getComponent<CameraComponent>() && element->getComponent<FreeCameraControllerComponent>())
+            {
+                MovementComponent *move = element->getComponent<MovementComponent>();
+                move->linearVelocity = glm::vec3(0, 0, -5 *2* this->level);
+            }
             if (entityData.contains("children"))
             {
                 // TODO: (Req 8) Recursively call this world's "deserialize" using the children data
