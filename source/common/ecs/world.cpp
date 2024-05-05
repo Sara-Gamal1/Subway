cbf990684362b82b28c0e2fbb50b0ab5da091c33
nesma-shafie <nesmashafie342@gmail.com>
2024-05-05 10:48:32 +0300

    ✨ using duplicates to render many entities

--- source/common/ecs/world.cpp
+++ source/common/ecs/world.cpp
@@ -2,7 +2,7 @@
 #include "../common/components/movement.hpp"
 #include "../common/components/player.hpp"
 #include <iostream>
-
+#include <vector>
 namespace our
 {
 
@@ -22,13 +22,13 @@ namespace our
             if (element->getComponent<MovementComponent>() && element->getComponent<PlayerComponent>())
             {
                 MovementComponent *move = element->getComponent<MovementComponent>();
-                move->linearVelocity = glm::vec3(0, 0, -5 *2* this->level);
+                move->linearVelocity = glm::vec3(0, 0, -5 * 2 * this->level);
             }
 
             if (element->getComponent<CameraComponent>() && element->getComponent<FreeCameraControllerComponent>())
             {
                 MovementComponent *move = element->getComponent<MovementComponent>();
-                move->linearVelocity = glm::vec3(0, 0, -5 *2* this->level);
+                move->linearVelocity = glm::vec3(0, 0, -5 * 2 * this->level);
             }
             if (entityData.contains("children"))
             {
@@ -37,7 +37,37 @@ namespace our
                 nlohmann::json children_data = entityData["children"];
                 World::deserialize(children_data, element);
             }
+            if (entityData.contains("duplicates"))
+            {
+                // glm::vec3 duplicates = glm::vec3(entityData.value("duplicates", duplicates));
+                nlohmann::json duplicates = entityData["duplicates"];
+                for (int i = 1; i <= (int)duplicates[0]; ++i)
+                {
+                    Entity *newDuplicateEntity = add();  // create a new entity using the add function in world.hpp
+                    newDuplicateEntity->parent = parent; // set the parent of the new entity to the given parent
+                    newDuplicateEntity->deserialize(     // deserialize the new entity using the given entityData
+                    entityData);
+                    newDuplicateEntity->localTransform.position.x += int(i) * (int)duplicates[1];
+                }
+
+                for (int i = 1; i <= (int)duplicates[2]; ++i)
+                {
+                    Entity *newDuplicateEntity = add();  // create a new entity using the add function in world.hpp
+                    newDuplicateEntity->parent = parent; // set the parent of the new entity to the given parent
+                    newDuplicateEntity->deserialize(     // deserialize the new entity using the given entityData
+                    entityData);
+                    newDuplicateEntity->localTransform.position.y += int(i) * (int)duplicates[3];
+                }
+
+                for (int i = 1; i <= (int)duplicates[4]; ++i)
+                {
+                    Entity *newDuplicateEntity = add();  // create a new entity using the add function in world.hpp
+                    newDuplicateEntity->parent = parent; // set the parent of the new entity to the given parent
+                    newDuplicateEntity->deserialize(     // deserialize the new entity using the given entityData
+                    entityData);
+                    newDuplicateEntity->localTransform.position.z += int(i) * (int)duplicates[5];
+                }
+            }
         }
     }
-
 }
